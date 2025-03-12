b1bb415a1d6b9a255dea497ec98e57b1fa0c19c1
fldus <fldus@DESKTOP-H7CU5SP>
2025-03-12 14:04:53 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;   // using : import
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld    // namespace : 패키지
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
